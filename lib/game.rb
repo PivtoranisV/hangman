5f9eed57acb088cfdea1994d68dff77074cddfa5
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-09-24 12:36:49 +0300

    Added method to display secret word with dashes

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
+require_relative 'dictionary'
 class Game
+  def initialize
+    puts 'Welcome to Hangman!'
+    puts 'Guess the secret word. You have 6 wrong guesses.'
+    @secret_word = Dictionary.new.select_secret_word
+    @incorrect_guesses = 0
+    @correct_guesses = []
+    puts display_secret_word_dashes
+  end
+
+  private
+
+  def display_secret_word_dashes
+    @secret_word.gsub(/[^A-Z\s]/, '_ ')
+  end
 end
--- main.rb
+++ main.rb
@@ -4,5 +4,4 @@ require_relative 'lib/game'
 require_relative 'lib/player'
 require_relative 'lib/dictionary'
 
-d = Dictionary.new
-p d.select_secret_word
+Game.new
