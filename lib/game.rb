3cc8457a6e524ed8355d1d3330d09382df7a1e90
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-09-24 11:28:15 +0300

    Added load_dictionary method

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Game
+  def load_dictionary
+    File.open('dictionary.txt') do |file|
+      file.readlines.map(&:chomp)
+    end
+  end
 end
--- main.rb
+++ main.rb
@@ -2,3 +2,6 @@
 
 require_relative 'lib/game'
 require_relative 'lib/player'
+g = Game.new
+words = g.load_dictionary
+puts words.first
